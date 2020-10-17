fd4d7510696ad2dad73322b6cfa98d65ca55213f
Your Name <you@example.com>
2020-10-17 09:13:54 +0200

    some changes in style

--- src/Components/Layout/Headernav.js
+++ src/Components/Layout/Headernav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Button } from "antd";
 import "../../Styling/Headernav.css";
 const { Header, Content, Footer } = Layout;
 
@@ -7,12 +7,51 @@ export default function Headernav() {
   return (
     <Layout className="layout">
       <Header id="home-header">
-        <div className="logo" />
-        <Menu id="home-menu" mode="horizontal" defaultSelectedKeys={["2"]}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+        <div className="logo">
+          <img className="logo-img" src="/images/logo.png" alt="logo" />
+        </div>
+        <Menu id="home-menu" mode="horizontal" defaultSelectedKeys={["1"]}>
+          <Menu.Item className="home-menu-item home-menu-item-active" key="1">
+            Home
+          </Menu.Item>
+          <Menu.Item className="home-menu-item " key="2">
+            Services
+          </Menu.Item>
+          <Menu.Item className="home-menu-item" key="3">
+            About us
+          </Menu.Item>
+          <Menu.Item className="home-menu-item" key="4">
+            Contact
+          </Menu.Item>
+          <Button
+            className="login-btn"
+            style={{
+              backgroundColor: "#F8B544",
+              color: "white",
+              border: "none",
+            }}
+          >
+            Login
+          </Button>
         </Menu>
+        <div className="header-content">
+          <div className="empty-div-right"></div>
+          <h1>
+            Book Your <span style={{ color: "##F8B544" }}> Chalet</span>
+          </h1>
+          <p>From your Home</p>
+          <div className="empty-div-left"></div>
+        </div>
+        <div className="wave">
+          <img className="wave-img" src="/images/wavees.png" alt="wave" />
+        </div>
+        {/* <div className="side-wave">
+          <img
+            className="side-wave-img"
+            src="/images/Side wave.png"
+            alt="wave"
+          />
+        </div> */}
       </Header>
       <Content id="home-content">
         <div className="site-layout-content"></div>
