b41eecc5d71a4e9597501a9d2786568cb3ec194c
Your Name <you@example.com>
2021-01-09 12:55:18 +0200

    the same of the previeous commit

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -2,35 +2,9 @@ import React from "react";
 import "../Styling/footer.css";
 import { Row, Col } from "antd";
 import { Link } from "react-router-dom";
-// const { Footer } = Layout;
 
 export default function Foter() {
   return (
-    //   <Footer className="text-white">
-    //  <div>
-    //     <span className=" h1"> Stay in touch</span>
-    //     <div className="ml-4 mt-3 h3">
-    //       <i class="fab fa-facebook-square mr-2"></i>
-    //       <i class="fab fa-google-plus mr-2"></i>
-    //       <i class="fab fa-twitter mr-2"></i>
-    //       <i class="fab fa-instagram mr-2"></i>
-    //       </div>
-    //       </div>
-    //   <div>
-    //   <span className="font-weight-bold h2 mb-3">Contact</span>
-    //   <br />
-    //   <span>
-    //   <i class="fas fa-phone-alt mr-2"></i>000-000-0000
-    //     </span>
-    //     <span>
-    //     <i class="fas fa-envelope mr-2 ml-2"></i>info@email.com
-    //     </span>
-    //     <br />
-    //     <span>
-    //     <i class="fas fa-map-marker-alt mr-2"></i>Cairo , Egypt
-    //     </span>
-    //   </div>
-    //     </Footer>
     <div className="footer-container">
       <Row>
         <Col span={12}>
